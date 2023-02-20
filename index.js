2a71851ff4776f527298daf37c25152cd1f066bf
mdefat233 <kharifte16@gmail.com>
2023-02-20 22:42:19 +0600

    news api added

--- index.js
+++ index.js
@@ -5,13 +5,33 @@ const express = require('express')
 
  app.use(cors());
  const categories = require('./data/categories.json');
+ const news = require('./data/news.json');
 
  app.get('/', (req, res) => {
     res.send('News API Running');
  });
  app.get('/news-categories', (req, res) =>{
-    res.send(categories);
+   res.send(categories);
+ });
+ app.get('/news/:id', (req, res) =>{
+   // console.log(req.params.id)
+   const id = req.params.id;
+   const selectedNews = news.find(n => n._id === id);
+   res.send(selectedNews);
  })
+ app.get('/category/:id', (req, res) =>{
+   const id = req.params.id;
+   if(id ==='08'){
+      res.send(news);
+   }
+   else{
+      const category_news = news.filter( n=> n.category_id === id);
+      res.send(category_news);
+   }
+  
+ })
+ 
+
  app.listen(port, () =>{
     console.log('news portal server running', port);
  })
